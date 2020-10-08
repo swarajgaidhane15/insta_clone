e34fe0236f9b91f63fc2405a6a6feda3adc3833d
swarajgaidhane15 <rutvikgaidhane1508@gmail.com>
2020-10-09 00:27:41 +0530

    Deployed

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "YOUR API KEY",
-  authDomain: "YOUR authDomain",
-  databaseURL: "YOUR databaseURL",
-  projectId: "YOUR projectId",
-  storageBucket: "YOUR storageBucket",
-  messagingSenderId: "YOUR messagingSenderId",
-  appId: "YOUR appId",
-  measurementId: "YOUR measurementId",
+  apiKey: "AIzaSyALURnJcAZY2wE5cOmaG0HxHwhWdCrPbyk",
+  authDomain: "instashare-potts.firebaseapp.com",
+  databaseURL: "https://instashare-potts.firebaseio.com",
+  projectId: "instashare-potts",
+  storageBucket: "instashare-potts.appspot.com",
+  messagingSenderId: "455975184209",
+  appId: "1:455975184209:web:aa8f4c561cb3d323d85570",
+  measurementId: "G-T5KEDHC7FS",
 });
 
 const db = firebaseApp.firestore();
